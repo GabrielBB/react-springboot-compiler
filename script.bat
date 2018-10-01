d7d9c5eb573818a8f03e0f95895f7b707ad6e25b
Gabriel Basilio Brito <gabrielbb0306@gmail.com>
2018-10-01 17:22:16 -0400

    Initial commit for the script batch file

---
+++ script.bat
@@ -0,0 +1,24 @@
+SET REACT_APP=
+SET SPRING_BOOT_APP=
+
+
+rem Building React app
+cd %REACT_APP%
+CALL npm run build
+
+rem Copying React build files to Spring Boot project. Spring Boot serves content in \src\main\resources\public by default.
+
+xcopy "%REACT_APP%\build\*" "%SPRING_BOOT_APP%\src\main\resources\public\*" /s /e /i /Y
+
+rem Compile Spring Boot application using Maven
+cd "%SPRING_BOOT_APP%"
+CALL mvn package
+
+rem As React build files change everytime we build the react app, we should not keep them in our Spring Boot project. Removing:
+rmdir /s /q  "%SPRING_BOOT_APP%\src\main\resources\public"
+
+rem Removing React build files from React Project
+rmdir /s /q  "%REACT_APP%\build"
+
+rem Compilation done.
+pause
